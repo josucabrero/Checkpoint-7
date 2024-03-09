f3c31c7cecd1cbed70774ff403850aa17e7b48ce
Josu Cabrero <150235461+josucabrero@users.noreply.github.com>
2024-03-09 18:48:06 +0100

    Add files via upload

---
+++ Checkpoint7.js
@@ -0,0 +1,8 @@
+function calculo(num1,num2,num3,num4){
+
+    if ((num1 + num2)*(num3 + num4) > 50) {
+        console.log("¡El numero es mayor que 50!");
+    } else {
+        console.log("¡El número es menor que 50!");
+    }
+}
